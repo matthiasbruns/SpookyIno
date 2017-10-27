ae3fe5172275e30823959a6311c2aa457a1298e2
matthiasbruns <matthiasbruns@live.de>
2017-10-27 22:04:11 +0200

    Inventory P1 done

--- Assets/Scripts/Inventory/InventoryComponent.cs
+++ Assets/Scripts/Inventory/InventoryComponent.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryComponent : MonoBehaviour {
+	public InventoryItemList database;
+
 	private List<InventorySlot> slots;
 
+	void Awake(){
+		slots = new List<InventorySlot>();
+		database = GameManager.Instance.itemDatabase;
+	}
+
+	// UNITY
+	void OnTriggerEnter2D(Collider2D other) {
+		var itemOwner = other.gameObject.GetComponent<ItemComponent>() as HasInventoryItem;
+        if(itemOwner != null){
+			AddItem(itemOwner.ItemId, itemOwner.Amount);
+			GameObject.Destroy(other.gameObject);
+		}
+    }
+
+	// INVENTORY
 	private InventorySlot GetSlot(InventoryItem item){
 		foreach(InventorySlot slot in slots){
 			if(slot.item?.itemName == item.itemName) {
@@ -23,17 +40,20 @@ public class InventoryComponent : MonoBehaviour {
 		return slot.item.isStackable && slot.item.encumbranceValue < slot.amount + amount;
 	}
 
+	public void AddItem(int itemId, int amount = 1){
+		InventoryItem item = database.itemList[itemId];
+		AddItem(item, amount);
+	}
+
 	public void AddItem(InventoryItem item, int amount = 1){
-		if(item.isStackable){
-			var slot = GetSlot(item);
-			// We need to check, if we have to change an existing slot
-			if(ShouldStackItem(slot, amount)){
-				// Stack item
-				slot.amount += amount;
-			} else {
-				// Create new ItemSlot
-				slots.Add(new InventorySlot(item, amount));
-			}
+		var slot = GetSlot(item);
+		// We need to check, if we have to change an existing slot
+		if(ShouldStackItem(slot, amount)){
+			// Stack item
+			slot.amount += amount;
+		} else {
+			// Create new ItemSlot
+			slots.Add(new InventorySlot(item, amount));
 		}
 	}
 }
