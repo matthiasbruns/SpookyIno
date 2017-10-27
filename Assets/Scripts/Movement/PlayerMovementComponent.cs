4c319c14d1333532cc71138c28364df4c2736d29
Maik Macho <nuda1998@gmail.com>
2017-10-27 21:22:58 +0200

    Add "hand" to PlayerMovementComponent

--- Assets/Scripts/Movement/PlayerMovementComponent.cs
+++ Assets/Scripts/Movement/PlayerMovementComponent.cs
@@ -9,6 +9,8 @@ public class PlayerMovementComponent : MonoBehaviour {
     [Range(0f, 1f)]
     public float SpeedDragFactor = 0.5f;
 
+    public Transform hand;
+
     Rigidbody2D body;
 
 	void Start() {
@@ -17,11 +19,14 @@ public class PlayerMovementComponent : MonoBehaviour {
             // Physikalisch inkorrekt, Spieler hält aber (fast) sofort an, fühlt sich besser an.
             body.drag = Speed * SpeedDragFactor;
         }
-	}
-	
-	void Update() {
+        if (hand == null)
+            hand = transform.Find("Hand"); // Jemand war faul und hat hand nicht gesetzt...
+    }
+
+    void Update() {
         float x = Input.GetAxis("Horizontal") * Speed;
         float y = Input.GetAxis("Vertical") * Speed;
+
         // Physikalisch inkorrekt, fühlt sich aber "snappier" an.
         if (x != 0)
             body.velocity = new Vector2(
@@ -35,6 +40,26 @@ public class PlayerMovementComponent : MonoBehaviour {
             );
         // Physikalisch korrekter.
         // body.AddForce(new Vector2(x, y), ForceMode2D.Force);
+
+        // Passe die Rotation der Hand an.
+        // TODO: Rechter Stick? Maus?
+        if (hand != null && (x != 0f || y != 0f)) {
+            if (Mathf.Abs(x) > Mathf.Abs(y)) {
+                // X "wichtiger"
+                if (x < 0f) {
+                    hand.rotation = Quaternion.Euler(180f, 90f, 0f);
+                } else if (x > 0f) {
+                    hand.rotation = Quaternion.Euler(0, 90f, 0f);
+                }
+            } else {
+                // Y "wichtiger"
+                if (y < 0f) {
+                    hand.rotation = Quaternion.Euler(90f, 90f, 0f);
+                } else if (y > 0f) {
+                    hand.rotation = Quaternion.Euler(-90, 90f, 0f);
+                }
+            }
+        }
 	}
 
 }
