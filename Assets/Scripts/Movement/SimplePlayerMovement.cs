61333cf680acc27c1198deb84c4fe52eca499429
Maik Macho <nuda1998@gmail.com>
2017-10-27 19:58:37 +0200

    SimplePlayerMovement

---
+++ Assets/Scripts/Movement/SimplePlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SimplePlayerMovement : MonoBehaviour {
+
+    public float Speed = 1f;
+
+    Rigidbody2D body;
+
+	void Start() {
+        body = GetComponent<Rigidbody2D>();
+        if (body.drag == 0f) {
+            body.drag = Speed; // Physikalisch inkorrekt, Spieler hält aber sofort an, fühlt sich besser an.
+        }
+	}
+	
+	void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        // Physikalisch inkorrekt, fühlt sich aber "snappier" an.
+        if (x != 0)
+            body.velocity = new Vector2(
+                x * Speed,
+                body.velocity.y
+            );
+        if (y != 0)
+            body.velocity = new Vector2(
+                body.velocity.x,
+                y * Speed
+            );
+	}
+
+}
