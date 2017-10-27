f1e59f9efe4a07a10c1fe1fcdefae7c808ea07a8
Maik Macho <nuda1998@gmail.com>
2017-10-27 20:48:01 +0200

    Add CameraFollowingComponent from an old project

--- Assets/Scripts/Movement/PlayerMovementComponent.cs
+++ Assets/Scripts/Movement/PlayerMovementComponent.cs
@@ -6,30 +6,35 @@ using UnityEngine;
 public class PlayerMovementComponent : MonoBehaviour {
 
     public float Speed = 10f;
+    [Range(0f, 1f)]
+    public float SpeedDragFactor = 0.5f;
 
     Rigidbody2D body;
 
 	void Start() {
         body = GetComponent<Rigidbody2D>();
         if (body.drag == 0f) {
-            body.drag = Speed; // Physikalisch inkorrekt, Spieler hält aber sofort an, fühlt sich besser an.
+            // Physikalisch inkorrekt, Spieler hält aber (fast) sofort an, fühlt sich besser an.
+            body.drag = Speed * SpeedDragFactor;
         }
 	}
 	
 	void Update() {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal") * Speed;
+        float y = Input.GetAxis("Vertical") * Speed;
         // Physikalisch inkorrekt, fühlt sich aber "snappier" an.
         if (x != 0)
             body.velocity = new Vector2(
-                x * Speed,
+                x,
                 body.velocity.y
             );
         if (y != 0)
             body.velocity = new Vector2(
                 body.velocity.x,
-                y * Speed
+                y
             );
+        // Physikalisch korrekter.
+        // body.AddForce(new Vector2(x, y), ForceMode2D.Force);
 	}
 
 }
