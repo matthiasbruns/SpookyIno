8cb8c82200774e52b3f78bf19e6c1ba8c04d3d6e
Maik Macho <nuda1998@gmail.com>
2017-10-28 02:24:07 +0200

    Add basic UI into game scene

---
+++ Assets/Scripts/UI/ArmorUIComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmorUIComponent : MonoBehaviour {
+
+    RectTransform rectTransform;
+
+    public HealthComponent Health;
+    public Image Panel;
+    public Text Text;
+
+    void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+
+        // Whoops I did it again.
+        if (Health == null)
+            Health = FindObjectOfType<HealthComponent>();
+        if (Panel == null)
+            Panel = transform.Find("Panel").GetComponent<Image>();
+        if (Text == null)
+            Text = transform.Find("Text").GetComponent<Text>();
+    }
+
+    void Update() {
+        rectTransform.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal,
+            ((RectTransform) transform.parent).sizeDelta.x * Health.maxArmor / (Health.maxHealth + Health.maxArmor)
+        );
+
+        Panel.rectTransform.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal,
+            ((RectTransform) Panel.transform.parent).sizeDelta.x * Health.currentArmor / Health.maxArmor
+        );
+        Text.text = $"AP: {Health.currentArmor} / {Health.maxArmor}";
+		Text.enabled = Health.maxArmor > 0;			
+    }
+
+}
