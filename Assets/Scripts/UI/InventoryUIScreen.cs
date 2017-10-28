238751061ae01d9a07ca00e1c8a8f3530517bc3f
Maik Macho <nuda1998@gmail.com>
2017-10-28 11:43:23 +0200

    Fix InventoryItem != null; Add UIScreenManager

---
+++ Assets/Scripts/UI/InventoryUIScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUIScreen : MonoBehaviour, IUIScreen {
+
+    public Image Darkening;
+    public RectTransform Panel;
+
+    public bool IsVisible {
+        get {
+            return Panel.gameObject.activeInHierarchy;
+        }
+        set {
+            Darkening.enabled = value;
+            Panel.gameObject.SetActive(value);
+        }
+    }
+
+    void Awake() {
+        if (Darkening == null)
+            Darkening = GetComponent<Image>();
+        if (Panel == null)
+            Panel = (RectTransform) transform.Find("Panel");
+    }
+
+    void Update() {
+
+    }
+
+}
