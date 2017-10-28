9ac5a88caed7f99f33f7ddb71228f0cc5e310507
MystaraCorvus <derdenker@live.de>
2017-10-28 22:11:11 +0200

    CharakterMation

--- Assets/Scripts/Player/PlayerActor.cs
+++ Assets/Scripts/Player/PlayerActor.cs
@@ -9,12 +9,18 @@ public class PlayerActor : MonoBehaviour, IActor {
     public Vector2 LookAngle => hand.forward;
 
     void Start() {
+
+        Animator amin = GetComponent<Animator>();
+        amin.SetFloat("LookDirectionX", LookAngle.x);
+        amin.SetFloat("LookDirectionY", LookAngle.y);
+        amin.SetFloat("Velocity", GetComponent<Rigidbody2D>().velocity.magnitude);
+
         if (hand == null)
-            hand = transform.Find("Hand"); // Jemand war faul und hat hand nicht gesetzt...
+            hand = transform.Find("Hand"); // Jemand war faul und hat hand nicht gesetzt..
     }
 	
 	void Update() {
-		
+
 	}
 
 }
