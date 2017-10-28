5a46bd24830f82bd6112e4707dc750cdd5a4c7c8
MystaraCorvus <derdenker@live.de>
2017-10-28 22:41:03 +0200

    Charakter Fixed

--- Assets/Scripts/Player/PlayerActor.cs
+++ Assets/Scripts/Player/PlayerActor.cs
@@ -7,19 +7,22 @@ public class PlayerActor : MonoBehaviour, IActor {
     public Transform hand;
 
     public Vector2 LookAngle => hand.forward;
+    Animator anim; 
 
     void Awake() {
         if (hand == null)
             hand = transform.Find("Hand"); // Jemand war faul und hat hand nicht gesetzt..
-
-        Animator amin = GetComponent<Animator>();
-        amin.SetFloat("LookDirectionX", LookAngle.x);
-        amin.SetFloat("LookDirectionY", LookAngle.y);
-        amin.SetFloat("Velocity", GetComponent<Rigidbody2D>().velocity.magnitude);
+        anim = GetComponent<Animator>();
+        anim.SetFloat("LookDirectionX", LookAngle.x);
+        anim.SetFloat("LookDirectionY", LookAngle.y);
+        anim.SetFloat("Velocity", GetComponent<Rigidbody2D>().velocity.magnitude);
     }
 	
 	void Update() {
+        anim.SetFloat("LookDirectionX", LookAngle.x);
+        anim.SetFloat("LookDirectionY", LookAngle.y);
+        anim.SetFloat("Velocity", GetComponent<Rigidbody2D>().velocity.magnitude);
 
-	}
+    }
 
 }
