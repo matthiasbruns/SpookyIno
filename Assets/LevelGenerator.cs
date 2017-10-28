a54d0835ee3743433a73ae899d82d8859a8b774d
MystaraCorvus <derdenker@live.de>
2017-10-28 08:08:08 +0200

    LevelGenFirstStep
    Floor and Level fixed. PathGeneration has a missing case

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    ///Constant
+    string objectTyp = "Level";
+    public GameObject FloorTile;
+
+    ///State
+    List<int> EmptyX = new List<int>();
+    List<int> EmptyY = new List<int>();
+    List<FloorGeneration> floorList = new List<FloorGeneration>();
+
+    ///Balancing
+    //Amount of floor tiles to be created
+    int Amount;
+    public float spawnInterval;
+
+    ///StartGenerating
+    FloorGeneration startFloor;
+
+
+    // Use this for initialization
+    void Start() {
+        Amount = 1999;
+        spawnInterval = 10f;
+
+        FloorGeneration startFloor = Instantiate(FloorTile, new Vector3(0, 0, 1), Quaternion.identity, transform).GetComponent<FloorGeneration>();
+        floorList.Add(startFloor);
+        startFloor.StartRoom();
+
+        EmptyX.Add(Mathf.RoundToInt(startFloor.transform.position.x));
+        EmptyY.Add(Mathf.RoundToInt(startFloor.transform.position.y));
+
+        StartCoroutine(SpawnNextFloor(spawnInterval));
+
+    }
+
+    public IEnumerator SpawnNextFloor(float time) {
+        yield return new WaitForSeconds(time);
+        if (EmptyX.Count >= Amount) yield break;
+
+        int lastX = EmptyX[EmptyX.Count - 1];
+        int lastY = EmptyY[EmptyY.Count - 1];
+        int nextX = lastX;
+        int nextY = lastY;
+
+
+        FloorGeneration oldObj = GetFloor(lastX, lastY);
+
+        string dir = oldObj.GetRandomExitDir();
+        if (dir == "N") nextY--;
+        if (dir == "S") nextY++;
+        if (dir == "E") nextX++;
+        if (dir == "W") nextX--;
+        //show_debug_message("skip: " + string(lastX) + " " + string(lastY));
+        //show_debug_message("nextOld: " + string(nextX) + " " + string(nextY));
+        //show_debug_message("nextNew: " + string(EmptyX[positionI - 1]) + " " + string(EmptyY[positionI - 1]));
+        FloorGeneration newObj = GetFloor(nextX, nextY);
+        if (newObj != null && newObj.GetProtected()) yield break; 
+
+        /*var dir=""
+        if (nextX>lastX) dir="W";
+        if (nextX<lastX) dir="E";
+        if (nextY>lastY) dir="N";
+        if (nextY<lastY) dir="S";*/
+
+        if (newObj == null) {
+            newObj = Instantiate(FloorTile, new Vector3(nextX * 16, nextY * 16, 1), Quaternion.identity, transform).GetComponent<FloorGeneration>();
+            floorList.Add(newObj);
+            floorList[floorList.Count - 1].lastX = nextX;
+            floorList[floorList.Count - 1].lastY = nextY;
+        }
+        newObj.ClearFloor();
+        newObj.GenExit();
+
+        EmptyX.Add(nextX);
+        EmptyY.Add(nextY);
+
+    }
+
+    public FloorGeneration GetFloor(int xPos, int yPos) {
+        xPos *= 16;
+        yPos *= 16;
+        foreach (FloorGeneration f in floorList) {
+            if (Mathf.RoundToInt(f.transform.position.x) == xPos && Mathf.RoundToInt(f.transform.position.y) == yPos) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ Assets/PathGeneration.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGeneration : MonoBehaviour {
+
+    string objectTyp;
+    FloorGeneration floorRef; 
+    public int pathIdx;
+    public int xPos { get; private set; }
+    public int yPos { get; private set; }
+
+    // Use this for initialization
+    void Awake() {
+        ///Constant
+        objectTyp = "Way";
+        floorRef = transform.parent.GetComponent<FloorGeneration>();
+        xPos = Mathf.RoundToInt(transform.position.x);
+        yPos = Mathf.RoundToInt(transform.position.y);
+
+        ///State
+        pathIdx = 0;
+    }
+
+    public void GenPath() {
+
+        List<string> spawnDirection = new List<string>();
+        int xPos = Mathf.RoundToInt(transform.position.x);
+        int yPos = Mathf.RoundToInt(transform.position.y);
+        PathGeneration exitColl;
+
+        exitColl = floorRef.GetPath(xPos + 1, yPos); 
+        if (exitColl == null && xPos < 15) {
+            spawnDirection.Add("E");
+        }
+        exitColl = floorRef.GetPath(xPos - 1, yPos);
+        if (exitColl == null && xPos > 0) {
+            spawnDirection.Add("W");
+        }
+        exitColl = floorRef.GetPath(xPos, yPos + 1);
+        if (exitColl == null && yPos < 15) {
+            spawnDirection.Add("S");
+        }
+        exitColl = floorRef.GetPath(xPos, yPos - 1);
+        if (exitColl == null && yPos > 0) {
+            spawnDirection.Add("N");
+        }
+
+        if (spawnDirection.Count > 0) {
+            int countDirectionTemp = Random.Range(0, spawnDirection.Count);
+            PathGeneration nextOne = null;
+
+            switch (spawnDirection[countDirectionTemp]) {
+                case ("E"): {
+                        nextOne = floorRef.CreateExit(xPos + 1, yPos, pathIdx);
+                        floorRef.instGenPath.Add(nextOne);
+                        break;
+                    }
+                case ("W"): {
+                        nextOne = floorRef.CreateExit(xPos - 1, yPos, pathIdx);
+                        floorRef.instGenPath.Add(nextOne);
+                        break;
+                    }
+                case ("S"): {
+                        nextOne = floorRef.CreateExit(xPos, yPos + 1, pathIdx);
+                        floorRef.instGenPath.Add(nextOne);
+                        break;
+                    }
+                case ("N"): {
+                        nextOne = floorRef.CreateExit(xPos, yPos - 1, pathIdx);
+                        floorRef.instGenPath.Add(nextOne);
+                        break;
+                    }
+                default: {
+                        break;
+                    }
+            }
+
+            if (nextOne != null) {
+                if (nextOne.xPos == 0) floorRef.hasExitW = true;
+                if (nextOne.yPos == 0) floorRef.hasExitN = true;
+                if (nextOne.xPos == 15) floorRef.hasExitE = true;
+                if (nextOne.yPos == 15) floorRef.hasExitS = true;
+            }
+            nextOne.CheckCollision();
+        }
+    }
+
+    void CheckCollision() {
+        PathGeneration exitIDOther;
+
+        exitIDOther = floorRef.GetPath(xPos + 1, yPos);
+        if (exitIDOther != null) {
+            floorRef.SetLinked(pathIdx, exitIDOther.pathIdx, true);
+        }
+        exitIDOther = floorRef.GetPath(xPos - 1, yPos);
+        if (exitIDOther != null) {
+            floorRef.SetLinked(pathIdx, exitIDOther.pathIdx, true);
+        }
+        exitIDOther = floorRef.GetPath(xPos, yPos - 1);
+        if (exitIDOther != null) {
+            floorRef.SetLinked(pathIdx, exitIDOther.pathIdx, true);
+        }
+        exitIDOther = floorRef.GetPath(xPos, yPos + 1);
+        if (exitIDOther != null) {
+            floorRef.SetLinked(pathIdx, exitIDOther.pathIdx, true);
+        }
+
+
+    }
+}
