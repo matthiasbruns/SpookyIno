b940e5f4f7e8b959151465b22e7a0eafc765c269
Maik Macho <nuda1998@gmail.com>
2017-10-29 09:13:21 +0100

    Fix lighting, game scene management

---
+++ Assets/Scripts/Lighting/SpriteShadow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SpriteShadow : MonoBehaviour {
+
+    public ShadowCastingMode ShadowCastingMode = ShadowCastingMode.Off;
+    public bool ReceiveShadows = true;
+
+    void Awake() {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.shadowCastingMode = ShadowCastingMode;
+        renderer.receiveShadows = ReceiveShadows;
+    }
+	
+	void Update () {
+		
+	}
+}
