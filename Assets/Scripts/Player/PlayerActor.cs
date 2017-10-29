be22ab1900ec0fcd57e409ab0838c4f1c585cbed
MystaraCorvus <derdenker@live.de>
2017-10-29 07:37:43 +0100

    AnimationCharakter Final

--- Assets/Scripts/Player/PlayerActor.cs
+++ Assets/Scripts/Player/PlayerActor.cs
@@ -10,6 +10,7 @@ public class PlayerActor : MonoBehaviour, IActor {
     Animator anim;
     Rigidbody2D Own;
 
+
     void Awake() {
         if (hand == null)
             hand = transform.Find("Hand"); // Jemand war faul und hat hand nicht gesetzt..
@@ -18,17 +19,11 @@ public class PlayerActor : MonoBehaviour, IActor {
     }
 	
 	void Update() {
+        
+        float angleLook = (float)((Mathf.Atan2(LookAngle.x, LookAngle.y) / Mathf.PI) * 180f) + 45f;
+        if (angleLook < 0) angleLook += 360f;
 
-        Vector2 fromVector2 = new Vector2(this.transform.position.x, this.transform.position.y);
-        Vector2 toVector2 = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-
-        float ang = Vector2.Angle(fromVector2, toVector2);
-        Vector3 cross = Vector3.Cross(fromVector2, toVector2);
-
-        if (cross.z > 0)
-            ang = 360 - ang;
-        float something = Own.velocity.magnitude;
-        anim.SetFloat("Velocity", something);
-        anim.SetFloat("Angle", ang);
+        anim.SetFloat("Velocity", Own.velocity.magnitude);
+        anim.SetFloat("Angle", angleLook);
     }
 }
